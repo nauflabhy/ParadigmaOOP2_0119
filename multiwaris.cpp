cc144b0d62a4ace09bebccfe171414ba0e2fac37
nauflabhy <n4uf4l.ku@gmail.com>
2025-06-03 18:30:39 +0700

    membuat class budi inheritance dengan class pekerja dan class pelajar

--- multiwaris.cpp
+++ multiwaris.cpp
@@ -28,3 +28,15 @@ class pelajar : virtual public orang {
                 cout << "Pelajar dibuat\n" << endl;
             }
 };
+
+class budi : public pekerja, public pelajar {
+    public:
+
+    budi(int pUmur) :
+        pekerja(pUmur),
+        pelajar(pUmur),
+        orang(pUmur) //Hal ini dapat dilakukan jika menggunakan virtual
+    {
+        cout << "Budi dibuat\n" << endl;
+    }
+};
