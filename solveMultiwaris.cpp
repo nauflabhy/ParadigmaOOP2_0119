beee50c89c37b45135f09beaf349fe35e2107be7
nauflabhy <n4uf4l.ku@gmail.com>
2025-06-03 18:39:43 +0700

    membuat class pekerja yang inheritance dengan class orang kali ini tanpa virtual

--- solveMultiwaris.cpp
+++ solveMultiwaris.cpp
@@ -11,3 +11,12 @@ class orang {
             cout << "Orang dibuat dengan umur " << umur << "\n" << endl;
         }
 };
+
+class pekerja : public orang {
+    public:
+        pekerja(int pUmur) :
+            orang(pUmur)
+        {
+            cout << "Pekerja dibuat\n" << endl;
+        }
+};
