5295168bc8c9cd2b145f7abdb85b6fec81208567
nauflabhy <n4uf4l.ku@gmail.com>
2025-06-03 17:32:03 +0700

    membuat pointer

--- polimorpi.cpp
+++ polimorpi.cpp
@@ -28,5 +28,12 @@ int main () {
     joko a;
     lia b;
 
+    obyek = &a;
+    obyek->pesan();
+    obyek = &b;
+    obyek->pesan();
+    //a.seseorang::pesan();
+    //b.seseorang::pesan();
+    return 0;
 
 }
