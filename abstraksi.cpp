f4a39ba8d3f3bf963a74b2f46f9e578182fc1d10
nauflabhy <n4uf4l.ku@gmail.com>
2025-06-03 17:37:59 +0700

    output menampilkan nilai

--- abstraksi.cpp
+++ abstraksi.cpp
@@ -6,12 +6,16 @@ class AbstraksiKlas {
 
     public:
 
+        // method untuk mengisi nilai
+        // private member
         void setXY(string a, string b) {
             x = a;
             y = b;
         }
 
+        // menampilkan nilai
         void display() {
-            
+            cout << "x = " << x << endl;
+            cout << "y = " << y << endl;
         }
 };
