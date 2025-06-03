cc6b1308bf8a857cb0bb841351de4064050131b1
nauflabhy <n4uf4l.ku@gmail.com>
2025-06-03 18:41:13 +0700

    membuat class budi yang inheritance dengan class pekerja dan class pelajar

--- solveMultiwaris.cpp
+++ solveMultiwaris.cpp
@@ -29,3 +29,14 @@ class pelajar : public orang {
                 cout << "Pelajar dibuat\n" << endl;
             }
 };
+
+class budi : public pekerja, public pelajar {
+    public:
+
+    budi(int pUmur) :
+        pekerja(pUmur),
+        pelajar(pUmur)
+    {
+        cout << "Budi dibuat\n" << endl;
+    }
+};
